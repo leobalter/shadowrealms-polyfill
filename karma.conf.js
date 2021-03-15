8f6a40100505eef1881e650b4045a4422ff35130
Leo Balter <leonardo.balter@gmail.com>
2021-03-15 09:48:19 -0700

    Get Karma and Babel to work together

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,24 @@
-module.exports = function(config) {
-  config.set({
-    basePath: '',
-    frameworks: ['qunit'],
-    files: [
-      'src/**/*.js',
-      'test/**/*.js'
-    ],
-    preprocessors: {
-      'src/**/*.js': ['babel'],
-      'test/**/*.js': ['babel']
-    },
-    reporters: ['progress', 'coverage'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['ChromeHeadless'], // , 'Firefox', 'Safari'],
-    singleRun: false,
-    concurrency: Infinity,
-    babelPreprocessor: {
-      options: {
-        sourceMap: 'inline'
-      },
-      filename: function (file) {
-        return file.originalPath.replace(/\.js$/, '.es5.js');
-      },
-      sourceFileName: function (file) {
-        return file.originalPath;
-      }
-    }
-  });
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['qunit'],
+        files: ['src/**/*.js', 'test/**/*.js'],
+        preprocessors: {
+            'src/**/*.js': ['babel'],
+            'test/**/*.js': ['babel']
+        },
+        reporters: ['progress', 'coverage'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless', 'FirefoxHeadless', 'Safari'],
+        singleRun: false,
+        concurrency: Infinity,
+        plugins: ['karma-*', '@onslip/karma-safari-launcher'],
+        babelPreprocessor: {
+            filename: ({ originalPath }) => originalPath.replace(/\.js$/, '.es5.js'),
+            sourceFileName: ({ originalPath }) => originalPath
+        }
+    });
 };
