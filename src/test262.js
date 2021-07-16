f43891ede8add32b48235b255174a55bec7799bb
rwaldron <waldron.rick@gmail.com>
2021-07-16 12:09:27 -0400

    fix: throw on invalid realm object

--- src/test262.js
+++ src/test262.js
@@ -55,6 +55,11 @@
         }
 
         evaluate(sourceText) {
+            try {
+                this.#realm;
+            } catch (error) {
+                throw new TypeError("Cross-Realm Error: invalid realm object");
+            }
             const string = String(sourceText);
             return this.#errorCatcher(() => this.#evaluateInRealm(string));
         }
